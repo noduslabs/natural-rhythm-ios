70dab41c0cc988b03aa68ecdc30d5f840ef9a2a6
Dmitry Paranyushkin <dmitry@noduslabs.com>
2025-07-07 22:31:51 +0200

    Initial Commit

---
+++ RhythmHaptics/RhythmHapticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RhythmHapticsApp.swift
+//  RhythmHaptics
+//
+//  Created by Dmitry Paranyushkin on 07/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct RhythmHapticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
